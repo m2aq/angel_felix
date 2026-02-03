7eb08b503928477818bd647df64f430df40e4a5f
MARIO ALBERTO ARMENTA QUINTEROS <9JLCF@cfe.mx>
2026-02-03 12:25:48 -0700

    Agregar configuracion de despliegue automatico

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/angel_felix/",
   server: {
     host: "::",
     port: 8080,
